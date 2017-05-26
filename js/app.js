d440b15038153f0016e7d5ca944307ac3a54a74c
alumne <alumne@alumne>
2017-05-26 21:34:22 +0200

    PHP Code started.

--- js/app.js
+++ js/app.js
@@ -66,7 +66,7 @@ function loginColumn (){
       controllerAs: 'mainView'
     };
   });
-  
+
    BioLifeApp.directive("contactView", function (){
     return {
       restrict: 'E',
@@ -77,7 +77,7 @@ function loginColumn (){
       controllerAs: 'contactView'
     };
   });
-  
+
   BioLifeApp.directive("articlesView", function (){
     return {
       restrict: 'E',
@@ -88,4 +88,53 @@ function loginColumn (){
       controllerAs: 'articlesView'
     };
   });
+
+  BioLifeApp.factory('userConnected', function(){
+
+      var user = new User();
+      return user;
+  });
+
+  BioLifeApp.directive('validFile',function(){
+      return {
+          require:'ngModel',
+          link:function(scope,el,attrs,ctrl){
+              ctrl.$setValidity('validFile', el.val() != '');
+              //change event is fired when file is selected
+              el.bind('change',function(){
+                  ctrl.$setValidity('validFile', el.val() != '');
+                  scope.$apply(function(){
+                      ctrl.$setViewValue(el.val());
+                      ctrl.$render();
+                  });
+              });
+          }
+      }
+  });
+
+  BioLifeApp.factory('accessService', function($http, $log, $q) {
+  	return {
+  		getData: function(url, async, method, params, data) {
+  			var deferred = $q.defer();
+  			$http({
+  				url: url,
+  				method: method,
+  				asyn: async,
+  				params: params,
+  				data: data
+  			})
+  			.success(function(response, status, headers, config)  {
+  				deferred.resolve(response);
+  			})
+  			.error(function(msg, code) {
+  				deferred.reject(msg);
+  				$log.error(msg, code);
+  				alert("There has been an error in the server, try later");
+  			});
+
+  			return deferred.promise;
+  		}
+  	}
+  });
+
 })();
